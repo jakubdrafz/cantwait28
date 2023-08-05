739300579de6cb9b8406feea8f1d9c3a8d51b178
Jakub Drafz <drafzjakub@gmail.com>
2023-08-05 21:23:02 +0200

    implement Item Model

--- lib/features/home/cubit/home_state.dart
+++ lib/features/home/cubit/home_state.dart
@@ -1,12 +1,12 @@
 part of 'home_cubit.dart';
 
 class HomeState {
-  const HomeState({
-    this.items,
+   const HomeState({ 
+    this.items = const [],
     this.loadingErrorOccured = false,
     this.removingErrorOccured = false,
   });
-  final QuerySnapshot<Map<String, dynamic>>? items;
+  final List<ItemModel> items;
   final bool loadingErrorOccured;
   final bool removingErrorOccured;
 }
