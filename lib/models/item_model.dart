1d0a34d780cab96794f029578c160a6c9a2c1d68
Jakub Drafz <drafzjakub@gmail.com>
2023-08-18 10:49:53 +0200

    Separate logic to items repository

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -1,9 +1,16 @@
 class ItemModel {
   ItemModel(
-      {required this.id, required this.title, required this.imageURL, required this.relaseDate});
+      {required this.id,
+      required this.title,
+      required this.imageURL,
+      required this.releaseDate});
 
   final String id;
   final String title;
   final String imageURL;
-  final DateTime relaseDate;
+  final DateTime releaseDate;
+
+  String daysLeft() {
+    return releaseDate.difference(DateTime.now()).inDays.toString();
+  }
 }
